f2bca4651e90f168fec0b55260a69d0546bdc4d0
Axn-Switch <dietespifftimothyswift@gmail.com>
2024-03-19 20:26:12 +0100

    first designs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import img from './womanlyingdown.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <div id='mean'>
+      <img src={img} className='max-w-2xl m-auto'/>
+      <h1>
+        Diete-Spiff Timothy
+      </h1>
+    </div>
     </>
   )
 }
