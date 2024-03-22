342c9f9dc2455b02776d0697b8c4ecf5d1a5b1a9
Axn-Switch <dietespifftimothyswift@gmail.com>
2024-03-22 20:35:24 +0100

    adding preloader for brief statements

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import './App.css'
-import img from './womanlyingdown.png'
 
 function App() {
 
   return (
     <>
-    <div id='mean'>
-      <img src={img} className='max-w-2xl m-auto'/>
-      <h1>
-        Diete-Spiff Timothy
-      </h1>
-    </div>
+      <div id='mean'>
+        <p>Hi, There</p>
+        <p className='text-5xl mb-5 mt-4'>I am <b className='text-purple-400'> Diete-Spiff Timothy</b> </p>
+        <p> and I build stuff for the web</p>
+      </div>
     </>
   )
 }
---
+++ src/components/preloader.tsx
@@ -0,0 +1,45 @@
+
+
+
+
+function preloader() {
+
+    const quotes = [
+        { 
+            quote: "A true passion that burns within your soul is one that can never be put out",
+            author: "Zach Toelke"
+        },
+        { 
+            quote: "Love what you do and do what you love. ",
+            author: "David Cuschieri"
+        },
+        { 
+            quote: "Passion is the genesis of genius.",
+            author: "Tony Robbins"
+        },
+        { 
+            quote: "The very basis of a man’s living spirit is his passion for adventure.",
+            author: "Christopher McCandless"
+        },
+        { 
+            quote: "Passion is the genesis of genius.",
+            author: "Tony Robbins"
+        },
+    ]
+
+    const res = () => {
+        
+        console.log(quotes[(Math.floor(Math.random() * quotes.length))]);
+    }
+  return (
+    <>
+        <div>
+            <p className=""></p>
+            <p ></p>
+        </div>
+
+    </>
+  )
+}
+
+export default preloader
